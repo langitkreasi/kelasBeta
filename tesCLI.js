d7be438b25d358e318d9312ef4c06a0e4d51ed9c
lintang kresnadi <langitkreasiid@gmail.com>
2021-06-17 15:18:25 +0700

    masukkan kode lurah

---
+++ tesCLI.js
@@ -0,0 +1,11 @@
+function cariLurahBaru(calons) {
+    // your code here
+  }
+  
+  console.log(cariLurahBaru([1, 2, 3, 2, 1, 3, 2, 3])); // pemilihan ulang
+  console.log(cariLurahBaru([1, 2, 3, 2, 1, 3, 3])); // 3
+  console.log(cariLurahBaru([1])); // 1
+  console.log(cariLurahBaru([])); // -1
+
+
+  //   ini ya temen2, semangatttt. di coba sebisanya, ngga harus solve, yg penting udah nyoba
